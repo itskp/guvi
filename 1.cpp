a52d770a64c91c0f6f2c50a5e0eac9a0ee1a841e
itskp <kapilgajarmal@gmail.com>
2017-03-01 16:29:16 +0530

    reverse string

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    string x;
+    cin>>x;
+
+    reverse(x.begin(),x.end());
+    cout<<x<<endl;
+    return 0;
+}
